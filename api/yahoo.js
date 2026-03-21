41fcc9ba793013bce18e13996901d5c10e80e8dc
nelsenp14 <prnelsen14@gmail.com>
2026-03-21 12:32:27 -0400

    Update yahoo.js

--- api/yahoo.js
+++ api/yahoo.js
@@ -1,5 +1,5 @@
-export default async function handler(req, res) {
-  const { symbol } = req.query;
+module.exports = async function handler(req, res) {
+  var symbol = req.query.symbol;
   if (!symbol) {
     return res.status(400).json({ error: "Missing symbol" });
   }
@@ -8,13 +8,13 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "GET");
 
   try {
-    const url = `https://query2.finance.yahoo.com/v8/finance/chart/${encodeURIComponent(symbol)}?range=5d&interval=1d&includePrePost=false`;
-    const r = await fetch(url, {
+    var url = "https://query2.finance.yahoo.com/v8/finance/chart/" + encodeURIComponent(symbol) + "?range=5d&interval=1d&includePrePost=false";
+    var r = await fetch(url, {
       headers: { "User-Agent": "Mozilla/5.0" }
     });
-    const data = await r.json();
+    var data = await r.json();
     res.status(200).json(data);
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
-}
+};
